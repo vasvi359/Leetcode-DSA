6e92b240784bc6b204f74d1928017af19db64bc8
vasvi-bali <vasvibali@gmail.com>
2026-09-19 14:45:17 +0530

    Implement Dutch National Flag sorting algorithm

---
+++ Dutch_national_flag.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void sortArray(vector<int>& arr, int n)
+{
+  int low = 0 ; int mid = 0 ; int high = n-1;
+
+  while(mid<=high){
+
+      if(arr[mid]==0){
+          swap(arr[mid], arr[low]);
+          mid++;
+          low++;
+      }
+      else if(arr[mid] == 1){
+          mid++;
+      }
+      else{
+          swap(arr[mid], arr[high]);
+              high--;
+          
+      }
+  }
+}
