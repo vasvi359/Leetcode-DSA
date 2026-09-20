cedc7d0e19f0a5ce92b13b6c5f9d749ada1692f6
vasvi-bali <vasvibali@gmail.com>
2026-09-20 23:59:30 +0530

    Create movezeeros.cpp

---
+++ movezeeros.cpp
@@ -0,0 +1,20 @@
+lass Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+    if(nums[i] == 0){
+
+        for(int j = i + 1; j < n; j++){
+
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                break;
+            }
+
+        }
+    }
+}
+        
+    }
+};
